8d29e21d25696c6a6bf12198af2e2bb99f1b511e
ryqzin <ryqzin.silva18@hotmail.com>
2025-06-20 21:40:40 +0000

    terminei o desafio notavo da batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
+int main(){
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    char coluna[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10'};
+    char tabuleiro[10][10];
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+    // Inicializa o tabuleiro com água
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = '0';
+        }
+    }
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Adiciona navios horizontais
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    for (int j = 5; j < 8; j++) { // navio de 3 posições na linha F
+        tabuleiro[5][j] = '3';
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // Adiciona navios verticais
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    for (int i = 7; i < 10; i++) { // navio de 3 posições na coluna 9
+        tabuleiro[i][8] = '3';
+    }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Exibe o cabeçalho das colunas
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", coluna[j]);
+    }
+    printf("\n");
+
+    // Exibe as linhas com o conteúdo do tabuleiro
+    for (int i = 0; i < 10; i++) {
+        printf("%c  ", linha[i]);
+        for (int j = 0; j < 10; j++) {
+            printf(" %c ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
